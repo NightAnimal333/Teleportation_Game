e4af2bd49a3ac350dfa1b5a24e8b37a842fd7758
NightAnimal <artursu@stud.ntnu.no>
2021-01-16 20:08:39 +0200

    Moar stuff

---
+++ Assets/Mouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetComponent<Renderer>().material.color = Color.white;
+
+        
+    }
+
+    void OnMouseEnter(){
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
---
+++ Assets/Teleport.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public Transform destination;
+    int canTeleport = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider){
+
+        print("TeleportHenlo");
+        print(collider.transform.position);
+        print(destination.position);
+
+        if (canTeleport >= 1)
+        {
+            collider.GetComponent<CharacterController>().enabled = false;
+            collider.transform.position = destination.position;
+            collider.GetComponent<CharacterController>().enabled = true;
+            canTeleport -= 2;
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider other) {
+        canTeleport += 1;
+    }
+
+}
